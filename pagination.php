8709e80d1f6b23501ba7ce735147f129aa6b28d4
KatePaladude <91999133+KatePaladude@users.noreply.github.com>
2021-10-06 03:36:44 +0300

    Update pagination.php

--- pagination.php
+++ pagination.php
@@ -1,42 +1,6 @@
 <?php
 $mysqli = new mysqli("localhost", "root", "", "test") or die ("Error");
 
-if(isset($_GET['order'])){
-    $order = $_GET['order'];
-}else{
-    $order = 'price';
-}
-
-if(isset($_GET['sort'])){
-    $sort = $_GET['sort'];
-} else{
-    $sort = 'ASC';
-}
-
-$resultSet = $mysqli->query("SELECT * FROM ads ORDER BY $order $sort");
-
-if($resultSet->num_rows >0){
-    echo"
-    <table border='1'>
-        <tr>
-            <th>Name</th> 
-            <th><a href='?order=price&&sort=$sort'>Price</a></th>
-            <th><a href='?order=date&&sort=$sort'>Date</a></th>
-    ";
-    while($rows = $resultSet->fetch_assoc())
-    {
-        $name=$row['name'];
-        $price=$row['price'];
-        $date=$row['created_at'];
-         echo"
-         <tr>
-            <td>$name</td>
-            <td>$price</td>
-            <td>$date</td>
-         ";
-    }
-    
-}
 if(!isset($_GET['page'])) $page = 1; else $page = htmlspecialchars($_GET['page']);
 if(ctype_digit($page) === false) $page = 1;
 
